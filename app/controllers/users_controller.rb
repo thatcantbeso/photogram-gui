2b838bed2945401d54133c39df5c1a6b07ac5fbd
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-10-06 21:34:20 +0000

    users done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
+  
   def index
     matching_users = User.all
 
     @list_of_users = matching_users.order({ :username => :asc })
 
   render ({ template: "user_templates/index"})
+  end
 
+  def show
+
+    # Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.first
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+      render ({ template: "user_templates/show"})
+    end
   end
 end
