9848680c088aab3ebae679cead652d18ec8a0561
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-10-09 14:52:35 +0000

    index

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,14 @@
 class PhotosController < ApplicationController
 
   def index
-    render ({ template: "photos_templates/index"})
+      matching_photos = Photo.all
+
+      @list_of_photos = matching_photos.order({ :created_at => :desc })
+
+    render({ template: "photos_templates/index"})
   end
 
   def show
-    render ({ template: "photos_templates/show"})
+    render({ template: "photos_templates/show"})
   end
 end
