1e3c783b75231353986f83fa052551cffbccc284
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-10-09 15:56:31 +0000

    photo create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -33,4 +33,22 @@ class PhotosController < ApplicationController
 
     redirect_to("/photos")
   end
+
+  def create
+# Parameters: {"query_image"=>"a", "query_caption"=>"b ", "query_owner_id"=>"c"}
+
+  input_image = params.fetch("query_image")
+  input_caption = params.fetch("query_caption")
+  input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+
+      redirect_to("/photos/" + a_new_photo.id.to_s )
+    #render({ template: "photos_templates/create"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ get("/users/:path_username", { :controller => "users", action: "show" })
 get("/photos", { :controller => "photos", action: "index"})
 get("/photos/:path_id", { :controller => "photos", action:"show"})
 get("/delete_photo/:toast_id", { :controller => "photos", action:"baii"})
+get("/insert_photo", { :controller => "photos", action:"create"})
 end
