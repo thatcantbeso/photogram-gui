c31954fb074137ffe245663c2f0c57b49271864e
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-10-09 20:35:29 +0000

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,4 +37,19 @@ class UsersController < ApplicationController
     #render ({ template: "user_templates/create"})
     redirect_to("/users/" + a_new_username.username.to_s)
   end
+  
+  def update
+#Parameters: {"input_username"=>"alfred", "modify_username"=>"123"}
+  the_id = params.fetch("modify_username")
+  matching_username = User.where({ :id => the_id })
+  the_user = matching_username.at(0)
+
+  input_user = params.fetch("input_username")
+
+  the_user.username = input_user
+
+  the_user.save
+
+redirect_to("/users/" + the_user.username.to_s)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ get("/", { :controller => "users", action: "index" })
 get("/users", { :controller => "users", action: "index" })
 get("/users/:path_username", { :controller => "users", action: "show" })
 post("/add_user", { :controller => "users", action: "create"})
+post("/update_user/:modify_username", { :controller => "users", action: "update"})
 
 get("/photos", { :controller => "photos", action: "index"})
 get("/photos/:path_id", { :controller => "photos", action:"show"})
